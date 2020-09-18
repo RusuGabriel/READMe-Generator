138cc9eb40c3cd655e18d379d9893811ed2216cf
Gabriel Rusu <rusugabriel13@gmail.com>
2020-09-18 19:16:17 +0300

    update: updated the project

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #! /usr/bin/python3
+from src import *
 from src.utils import *
 
+
 lines = parse('standard.md')
-result = []
+result_lines = []
 file = open("RESULT_README.md", "w", encoding="UTF-8")
 for line in lines:
-    if "${" in line:
-        firstPart = line.split("${")[0]
-        question = line.split("${")[1].split("}")[0]
-        secondPart = line.split("}")[1]
-        response = input(question)
-        line = firstPart + response + secondPart[1]
-    result.append(line +"\n")
-file.writelines(result)
+    tokens = parse_into_tokens(line)
+    for token in tokens:
+        response = input(token.question+": ")
+        line = replace(token, response, line)
+    result_lines.append(line +"\r")
+
+file.writelines(result_lines)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+from .structures import Token
 
 root = 'src'
 resources = 'resources'
@@ -10,3 +11,27 @@ def parse(template: str):
     for line in file.readlines():
         lines.append(line.strip())
     return lines
+
+
+def parse_into_tokens(line: str, start_index: int = 0):
+    if "${" not in line or len(line) < 3:
+        return []
+    start = line.index("${")
+    end = line.index("}")
+    multiline = False
+    if "*" in line:
+        end = line.index("*")
+        multiline = True
+    question = line[start+2:end]
+    collected_tokens =  [Token(question,multiline, start+2 + start_index,end + start_index)]
+    collected_tokens.extend(parse_into_tokens(line[end+1:], end + 1))
+    return collected_tokens
+
+def replace(token: Token,  word:str ,line: str):
+    return line[:token.start -2] + word + line[token.end+1:]
+
+
+# test_line = "something ${Few lines describing your project.} other thing ${cerva}"
+# tokens = parse_into_tokens(test_line)
+# for token in tokens:
+#     print(replace(token, "now", test_line))
